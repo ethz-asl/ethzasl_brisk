81851bd6e678abd0800ee34ee8f929e7d62f316f
Simon Lynen <simon.lynen@mavt.ethz.ch>
2013-04-04 10:57:32 +0200

    added matlab jet version of false color

--- brisk/src/false_color.cpp
+++ brisk/src/false_color.cpp
@@ -1,166 +1,255 @@
 /*
- * falsecolor.h
- *
- *  Created on: Oct 19, 2012
- *      Author: slynen
+
+ Copyright (c) 2013, Simon Lynen, ASL, ETH Zurich, Switzerland
+ You can contact the author at <slynen at ethz dot ch>
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of ETHZ-ASL nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL ETHZ-ASL BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
  */
-#include <opencv2/opencv.hpp>
 #include <falsecolor.h>
-#include <assert.h>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <opencv2/core/core.hpp>
 
 #define DEG2RAD 0.01745329
 palette GetPalette(palette::palettetypes pal)
 {
-	palette ret;
+  palette ret;
 
-    int i, r, g, b;
-    float f;
+  int i, r, g, b;
+  float f;
 
-    switch (pal)
-    {
-    	case palette::Linear_red_palettes:
-            /*
-             * Linear red palettes.
-             */
-            for (i = 0; i < 256; i++)
-            {
-                ret.colors[i].rgbBlue  = 0;
-                ret.colors[i].rgbGreen = 0;
-                ret.colors[i].rgbRed   = i;
-            }
-            break;
-        case palette::GammaLog_red_palettes:
-            /*
-             * GammaLog red palettes.
-             */
-            for (i = 0; i < 256; i++)
-            {
-                f = log10(pow((i/255.0), 1.0)*9.0 + 1.0) * 255.0;
-                ret.colors[i].rgbBlue  = 0;
-                ret.colors[i].rgbGreen = 0;
-                ret.colors[i].rgbRed   = f;
-            }
-            break;
-        case palette::Inversion_red_palette:
-            /*
-             * Inversion red palette.
-             */
-            for (i = 0; i < 256; i++)
-            {
-                ret.colors[i].rgbBlue  = 0;
-                ret.colors[i].rgbGreen = 0;
-                ret.colors[i].rgbRed   = 255 - i;
-            }
-            break;
-    	case palette::Linear_palettes:
-            /*
-             * Linear palettes.
-             */
-            for (i = 0; i < 256; i++)
-            {
-                ret.colors[i].rgbBlue  =
-                ret.colors[i].rgbGreen =
-                ret.colors[i].rgbRed   = i;
-            }
-            break;
-        case palette::GammaLog_palettes:
-            /*
-             * GammaLog palettes.
-             */
-            for (i = 0; i < 256; i++)
-            {
-                f = log10(pow((i/255.0), 1.0)*9.0 + 1.0) * 255.0;
-                ret.colors[i].rgbBlue  =
-                ret.colors[i].rgbGreen =
-                ret.colors[i].rgbRed   = f;
-            }
-            break;
-        case palette::Inversion_palette:
-            /*
-             * Inversion palette.
-             */
-            for (i = 0; i < 256; i++)
-            {
-                ret.colors[i].rgbBlue  =
-                ret.colors[i].rgbGreen =
-                ret.colors[i].rgbRed   = 255 - i;
-            }
-            break;
-        case palette::False_color_palette1:
-            /*
-             * False color palette #1.
-             */
-            for (i = 0; i < 256; i++)
-            {
-                r = (sin((i/255.0 * 360.0 + 0.0)   * DEG2RAD) * 0.5 + 0.5) * 255.0;
-                g = (sin((i/255.0 * 360.0 + 120.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
-                b = (sin((i/255.0 * 360.0 + 240.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
-                ret.colors[i].rgbBlue  = b;
-                ret.colors[i].rgbGreen = g;
-                ret.colors[i].rgbRed   = r;
-            }
-            break;
-        case palette::False_color_palette2:
-            /*
-             * False color palette #2.
-             */
-            for (i = 0; i < 256; i++)
-            {
-                r = (sin((i/255.0 * 360.0 + 120.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
-                g = (sin((i/255.0 * 360.0 + 240.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
-                b = (sin((i/255.0 * 360.0 + 0.0)   * DEG2RAD) * 0.5 + 0.5) * 255.0;
-                ret.colors[i].rgbBlue  = b;
-                ret.colors[i].rgbGreen = g;
-                ret.colors[i].rgbRed   = r;
-            }
-            break;
-        case palette::False_color_palette3:
-            /*
-             * False color palette #3.
-             */
-            for (i = 0; i < 256; i++)
-            {
-                r = (sin((i/255.0 * 360.0 + 240.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
-                g = (sin((i/255.0 * 360.0 + 0.0)   * DEG2RAD) * 0.5 + 0.5) * 255.0;
-                b = (sin((i/255.0 * 360.0 + 120.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
-                ret.colors[i].rgbBlue  = b;
-                ret.colors[i].rgbGreen = g;
-                ret.colors[i].rgbRed   = r;
-            }
-            break;
-    }
-    return ret;
+  switch (pal)
+  {
+    case palette::Linear_red_palettes:
+      /*
+       * Linear red palettes.
+       */
+      for (i = 0; i < 256; i++)
+      {
+        ret.colors[i].rgbBlue = 0;
+        ret.colors[i].rgbGreen = 0;
+        ret.colors[i].rgbRed = i;
+      }
+      break;
+    case palette::GammaLog_red_palettes:
+      /*
+       * GammaLog red palettes.
+       */
+      for (i = 0; i < 256; i++)
+      {
+        f = log10(pow((i / 255.0), 1.0) * 9.0 + 1.0) * 255.0;
+        ret.colors[i].rgbBlue = 0;
+        ret.colors[i].rgbGreen = 0;
+        ret.colors[i].rgbRed = f;
+      }
+      break;
+    case palette::Inversion_red_palette:
+      /*
+       * Inversion red palette.
+       */
+      for (i = 0; i < 256; i++)
+      {
+        ret.colors[i].rgbBlue = 0;
+        ret.colors[i].rgbGreen = 0;
+        ret.colors[i].rgbRed = 255 - i;
+      }
+      break;
+    case palette::Linear_palettes:
+      /*
+       * Linear palettes.
+       */
+      for (i = 0; i < 256; i++)
+      {
+        ret.colors[i].rgbBlue = ret.colors[i].rgbGreen = ret.colors[i].rgbRed = i;
+      }
+      break;
+    case palette::GammaLog_palettes:
+      /*
+       * GammaLog palettes.
+       */
+      for (i = 0; i < 256; i++)
+      {
+        f = log10(pow((i / 255.0), 1.0) * 9.0 + 1.0) * 255.0;
+        ret.colors[i].rgbBlue = ret.colors[i].rgbGreen = ret.colors[i].rgbRed = f;
+      }
+      break;
+    case palette::Inversion_palette:
+      /*
+       * Inversion palette.
+       */
+      for (i = 0; i < 256; i++)
+      {
+        ret.colors[i].rgbBlue = ret.colors[i].rgbGreen = ret.colors[i].rgbRed = 255 - i;
+      }
+      break;
+    case palette::False_color_palette1:
+      /*
+       * False color palette #1.
+       */
+      for (i = 0; i < 256; i++)
+      {
+        r = (sin((i / 255.0 * 360.0 - 120.0 > 0 ? i / 255.0 * 360.0 - 120.0 : 0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
+        g = (sin((i / 255.0 * 360.0 + 60.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
+        b = (sin((i / 255.0 * 360.0 + 140.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
+        ret.colors[i].rgbBlue = b;
+        ret.colors[i].rgbGreen = g;
+        ret.colors[i].rgbRed = r;
+      }
+      break;
+    case palette::False_color_palette2:
+      /*
+       * False color palette #2.
+       */
+      for (i = 0; i < 256; i++)
+      {
+        r = (sin((i / 255.0 * 360.0 + 120.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
+        g = (sin((i / 255.0 * 360.0 + 240.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
+        b = (sin((i / 255.0 * 360.0 + 0.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
+        ret.colors[i].rgbBlue = b;
+        ret.colors[i].rgbGreen = g;
+        ret.colors[i].rgbRed = r;
+      }
+      break;
+    case palette::False_color_palette3:
+      /*
+       * False color palette #3.
+       */
+      for (i = 0; i < 256; i++)
+      {
+        r = (sin((i / 255.0 * 360.0 + 240.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
+        g = (sin((i / 255.0 * 360.0 + 0.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
+        b = (sin((i / 255.0 * 360.0 + 120.0) * DEG2RAD) * 0.5 + 0.5) * 255.0;
+        ret.colors[i].rgbBlue = b;
+        ret.colors[i].rgbGreen = g;
+        ret.colors[i].rgbRed = r;
+      }
+      break;
+
+    case palette::False_color_palette4:
+      /*
+       * False color palette #4. Matlab JET
+       */
+
+      enum
+      {
+        nsep = 64, nvals = 192, n = 256
+      };
+
+      std::vector<double> vals;
+      vals.resize(nvals, 0);
+
+      int idx = 0;
+      for (int i = 0; i < nsep; ++i)
+      {
+        vals.at(idx++) = (i / (double)nsep);
+      }
+
+      for (int i = 0; i < nsep; ++i){
+        vals.at(idx + i) = 1.;
+      }
+
+      idx += nsep;
+      for (int i = nsep - 1; i >= 0; --i)
+      {
+        vals.at(idx++) = i / (double)nsep;
+      }
+
+      std::vector<int> r;
+      r.resize(nvals);
+      std::vector<int> g;
+      g.resize(nvals);
+      std::vector<int> b;
+      b.resize(nvals);
+      for (std::size_t i = 0; i < nvals; ++i)
+      {
+        g.at(i) = ceil(nsep / 2) - 1 + i;
+        r.at(i) = g.at(i) + nsep;
+        b.at(i) = g.at(i) - nsep;
+      }
+
+      int idxr = 0;
+      int idxg = 0;
+
+      for (int i = 0; i < nvals; ++i)
+      {
+        if (r.at(i) >= 0 && r.at(i) < n)
+          ret.colors[r.at(i)].rgbRed = vals.at(idxr++) * 255.;
+
+        if (g.at(i) >= 0 && g.at(i) < n)
+          ret.colors[g.at(i)].rgbGreen = vals.at(idxg++) * 255.;
+      }
+
+      int idxb = 0;
+      int cntblue = 0;
+      for (int i = 0; i < nvals; ++i)
+      {
+        if (b.at(i) >= 0 && b.at(i) < n)
+          cntblue++;
+      }
+
+      for (int i = 0; i < nvals; ++i)
+      {
+        if (b.at(i) >= 0 && b.at(i) < n)
+          ret.colors[b.at(i)].rgbBlue = vals.at(nvals - 1 - cntblue + idxb++) * 255.;
+      }
+      break;
+  }
+  return ret;
 }
-
 #undef DEG2RAD
-void convertFalseColor(const cv::Mat& srcmat, cv::Mat& dstmat, palette::palettetypes paltype){
 
-	palette pal = GetPalette(paltype);
+void convertFalseColor(const cv::Mat& srcmat, cv::Mat& dstmat, palette::palettetypes paltype)
+{
 
-	dstmat.create(srcmat.rows, srcmat.cols, CV_8UC3);
+  palette pal = GetPalette(paltype);
 
+  dstmat.create(srcmat.rows, srcmat.cols, CV_8UC3);
 
-	cv::Size sz = srcmat.size();
-	const uchar* src = srcmat.data;
-	uchar* dst = dstmat.data;
+  cv::Size sz = srcmat.size();
+  const unsigned char* src = srcmat.data;
+  unsigned char* dst = dstmat.data;
 
-	if( srcmat.isContinuous() && dstmat.isContinuous() )
-	{
-		sz.width *= sz.height;
-		sz.height = 1;
-	}
+  if (srcmat.isContinuous() && dstmat.isContinuous())
+  {
+    sz.width *= sz.height;
+    sz.height = 1;
+  }
 
-	for(int i = 0;i<sz.width;++i){
-		for(int j = 0;j<sz.height;++j){
-			int idx = j*sz.width + i;
-			uint8_t val = src[idx];
-			dst[idx*dstmat.channels() + 0] = pal.colors[val].rgbBlue;
-			dst[idx*dstmat.channels() + 1] = pal.colors[val].rgbGreen;
-			dst[idx*dstmat.channels() + 2] = pal.colors[val].rgbRed;
-		}
-	}
+  for (int i = 0; i < sz.width; ++i)
+  {
+    for (int j = 0; j < sz.height; ++j)
+    {
+      int idx = j * sz.width + i;
+      uint8_t val = src[idx];
+      dst[idx * dstmat.channels() + 0] = pal.colors[val].rgbBlue;
+      dst[idx * dstmat.channels() + 1] = pal.colors[val].rgbGreen;
+      dst[idx * dstmat.channels() + 2] = pal.colors[val].rgbRed;
+    }
+  }
 
 }
-
-
 
--- brisk/src/flir.cpp
+++ brisk/src/flir.cpp
@@ -5,8 +5,6 @@
  *      Author: lestefan
  */
 
-
-
 #include <opencv2/opencv.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <brisk/brisk.h>
@@ -41,8 +39,10 @@ cv::Mat descriptorsLast;
 std::vector<cv::KeyPoint> keypointsLast;
 cv::Mat img_mono8Last;
 
-class converter_16_8{
-  enum{
+class converter_16_8
+{
+  enum
+  {
     histminmembersperbucket = 10,
   };
 private:
@@ -51,25 +51,30 @@ private:
   static converter_16_8* inst_;
   bool firstframe_;
 public:
-  converter_16_8(){
+  converter_16_8()
+  {
     min_ = std::numeric_limits<uint16_t>::max();
     max_ = 0;
     firstframe_ = true;
   }
 
-  ~converter_16_8(){
+  ~converter_16_8()
+  {
     delete inst_;
     inst_ = NULL;
   }
 
-  static converter_16_8& Instance(){
-    if(!inst_){
+  static converter_16_8& Instance()
+  {
+    if (!inst_)
+    {
       inst_ = new converter_16_8;
     }
     return *inst_;
   }
 
-  void convert_to8bit(const cv::Mat& img16, cv::Mat& img8) {
+  void convert_to8bit(const cv::Mat& img16, cv::Mat& img8)
+  {
 
     uint16_t min = std::numeric_limits<uint16_t>::max();
     uint16_t max = 0;
@@ -78,28 +83,33 @@ public:
     typedef std::map<uint16_t, int> hist_t;
     hist_t hist;
 
-    for(int i=0;i<img16.cols;++i)
+    for (int i = 0; i < img16.cols; ++i)
     {
-      for(int j=0;j<img16.rows;++j)
+      for (int j = 0; j < img16.rows; ++j)
       {
-        uint16_t temp=img16.at<uint16_t>(j,i);
+        uint16_t temp = img16.at<uint16_t>(j, i);
         hist[temp]++;
       }
     }
 
     //find the main section of the histogram
-    for(hist_t::const_iterator it = hist.begin(); it!=hist.end() ; ++it){
-      if(it->second > histminmembersperbucket){
-        if(it->first > max){
+    for (hist_t::const_iterator it = hist.begin(); it != hist.end(); ++it)
+    {
+      if (it->second > histminmembersperbucket)
+      {
+        if (it->first > max)
+        {
           max = it->first;
         }
-        if(it->first < min){
+        if (it->first < min)
+        {
           min = it->first;
         }
       }
     }
 
-    if(firstframe_){
+    if (firstframe_)
+    {
       min_ = min;
       max_ = max;
     }
@@ -109,22 +119,20 @@ public:
     min_ = expsm * min_ + (1. - expsm) * min;
     max_ = expsm * max_ + (1. - expsm) * max;
 
-    for(int i=0;i<img16.cols;++i)
+    for (int i = 0; i < img16.cols; ++i)
     {
-      for(int j=0;j<img16.rows;++j)
+      for (int j = 0; j < img16.rows; ++j)
       {
-        double temp=(double)(img16.at<uint16_t>(j,i) - min_);
-        int val = ((temp/(max_ - min_))*255);
+        double temp = (double)(img16.at<uint16_t>(j, i) - min_);
+        int val = ((temp / (max_ - min_)) * 255);
         val = val > std::numeric_limits<uint8_t>::max() ? std::numeric_limits<uint8_t>::max() : val < 0 ? 0 : val; //saturate
-        img8.at<uint8_t>(j,i) = (uint8_t)val;
+        img8.at<uint8_t>(j, i) = (uint8_t)val;
       }
     }
 
     firstframe_ = false;
   }
 };
-
-
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -136,7 +144,6 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   try
   {
 
-
     // get image
 
 #if ROS_VERSION_MINIMUM(1, 9, 44)
@@ -147,11 +154,12 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     cv::Mat img = ptr->image;
 
     // only at first call
-    if(img_mono8Last.cols==0){
-      img_mono8Last=cv::Mat::zeros(img.rows,img.cols,CV_8UC1);
+    if (img_mono8Last.cols == 0)
+    {
+      img_mono8Last = cv::Mat::zeros(img.rows, img.cols, CV_8UC1);
     }
 
-    cv::Mat half_img(img.rows*2/3,img.cols*2/3,CV_16UC1);
+    cv::Mat half_img(img.rows * 2 / 3, img.cols * 2 / 3, CV_16UC1);
     //timeval start, end;
     //gettimeofday(&start, NULL);
     //for(int i=0; i<100; ++i)
@@ -165,80 +173,70 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     //cv::medianBlur(raw,img, 3);
     cv::Mat img_mono32F;
     cv::Mat half_img_mono32F;
-    img.convertTo(img_mono32F,CV_32F);
-    half_img.convertTo(half_img_mono32F,CV_32F);
+    img.convertTo(img_mono32F, CV_32F);
+    half_img.convertTo(half_img_mono32F, CV_32F);
 
     /*BRISKing*/
     // descriptors
     std::vector<cv::KeyPoint> keypoints;
-    detector->detect(img,keypoints);
+    detector->detect(img, keypoints);
 
     // extract
     cv::Mat descriptors;
-    extractor->compute(img,keypoints,descriptors);
+    extractor->compute(img, keypoints, descriptors);
 
     // match
     std::vector<std::vector<cv::DMatch> > matches;
-    matcher->radiusMatch(descriptorsLast,descriptors,
-                         matches,60);
+    matcher->radiusMatch(descriptorsLast, descriptors, matches, 50);
 
-    // make it reasonable for display
-    //	  double max;
-    //	  double min;
-    //	  cv::minMaxIdx(img_mono32F, &min,&max);
-    //	  img_mono32F=(img_mono32F-min)*(1.0/(max-min));
-    //	  //cv::minMaxIdx(half_img_mono32F, &min,&max);
-    //	  half_img_mono32F=(half_img_mono32F-min)*(1.0/(max-min));
-    //	  cv::Mat img_mono32F_scaled=img_mono32F*256.0;
-    //	  cv::Mat half_img_mono32F_scaled=half_img_mono32F*256.0;
-    //	  cv::Mat img_mono8;
-    //	  cv::Mat half_img_mono8;
-    //	  img_mono32F_scaled.convertTo(img_mono8,CV_8UC1);
-    //	  half_img_mono32F_scaled.convertTo(half_img_mono8,CV_8UC1);
 
+    //convert 8 bit and false color conversion
     static bool dofalsecolor = true;
-
 
     cv::Mat img_mono8_ir, img_mono8;
     img_mono8_ir.create(img.rows, img.cols, CV_8UC1);
     converter_16_8::Instance().convert_to8bit(img, img_mono8_ir);
-    if(dofalsecolor){
-      convertFalseColor(img_mono8_ir, img_mono8, palette::False_color_palette3);
-    }else{
+
+    if (dofalsecolor)
+    {
+      convertFalseColor(img_mono8_ir, img_mono8, palette::False_color_palette4);
+    }
+    else
+    {
       img_mono8 = img_mono8_ir;
     }
-    // draw stuff
+
+    // draw images
     cv::Mat drawing;
-    cv::drawKeypoints(img_mono8,keypoints,drawing,
-                      cv::Scalar(255,0,0),cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS); // blue,multisize
+    cv::drawKeypoints(img_mono8, keypoints, drawing, cv::Scalar(255, 0, 0), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS); // blue,multisize
 
     cv::Mat matchImg;
-    cv::drawMatches(img_mono8Last,keypointsLast,img_mono8,keypoints,matches,matchImg,
-                    cv::Scalar(0,255,0),cv::Scalar(255,0,0),
-                    std::vector<std::vector<char> >(),
-                    cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS|cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    cv::drawMatches(img_mono8Last, keypointsLast, img_mono8, keypoints, matches, matchImg, cv::Scalar(0, 255, 0),
+                    cv::Scalar(255, 0, 0), std::vector<std::vector<char> >(),
+                    cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS | cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
     // display
     cv::imshow("Raw", img_mono8);
-    cv::imshow("Matches",matchImg);
+    cv::imshow("Matches", matchImg);
     //cv::imshow("Half", half_img_mono8);
     cv::imshow("Points", drawing);
 
     char key = cv::waitKey(20);
-    if(key == 'c'){
+    if (key == 'c')
+    {
       dofalsecolor = !dofalsecolor;
     }
 
     // memory
-    descriptorsLast=descriptors;
-    keypointsLast=keypoints;
-    img_mono8Last=img_mono8;
+    descriptorsLast = descriptors;
+    keypointsLast = keypoints;
+    img_mono8Last = img_mono8;
     /*gettimeofday(&start, NULL);
-	  for(int i=0; i<100; ++i)
-	  brisk::ScaleSpaceLayer<brisk::HarrisScoreCalculatorFloat>::halfsample(img_mono8,half_img_mono8);
-	  gettimeofday(&end, NULL);
-	  dt = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/ 1000.;
-	  std::cout<<dt/100<<"ms mono8"<<std::endl;*/
+     for(int i=0; i<100; ++i)
+     brisk::ScaleSpaceLayer<brisk::HarrisScoreCalculatorFloat>::halfsample(img_mono8,half_img_mono8);
+     gettimeofday(&end, NULL);
+     dt = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/ 1000.;
+     std::cout<<dt/100<<"ms mono8"<<std::endl;*/
   }
 
 #if ROS_VERSION_MINIMUM(1, 9, 44)
@@ -255,17 +253,17 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "flir");
   std::string path = ros::package::getPath("brisk");
-  detector = new brisk::ScaleSpaceFeatureDetector<brisk::HarrisScoreCalculatorFloat>(2,14,130);
-  extractor = new cv::BriskDescriptorExtractor(path+"/brisk.ptn",true,true);
+  detector = new brisk::ScaleSpaceFeatureDetector<brisk::HarrisScoreCalculatorFloat>(2, 14, 80);
+  extractor = new cv::BriskDescriptorExtractor(path + "/brisk.ptn", false, true);
   matcher = new cv::BruteForceMatcherSse();
 
   ros::NodeHandle nh;
   cvNamedWindow("Raw");
   cvNamedWindow("Matches");
   cvNamedWindow("Points");
-  cvMoveWindow("Raw", 200,50);
-  cvMoveWindow("Matches", 200,350);
-  cvMoveWindow("Points", 550,50);
+  cvMoveWindow("Raw", 200, 50);
+  cvMoveWindow("Matches", 200, 350);
+  cvMoveWindow("Points", 550, 50);
 
   cvStartWindowThread();
   image_transport::ImageTransport it(nh);
