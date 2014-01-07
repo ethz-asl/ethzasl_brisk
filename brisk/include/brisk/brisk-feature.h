5241ada1a889152d9cb835a2458e32c73990f129
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-01-07 16:27:21 +0100

    Adding feature2d interface

---
+++ brisk/include/brisk/brisk-feature.h
@@ -0,0 +1,118 @@
+/*
+ Copyright (C) 2011  The Autonomous Systems Lab, ETH Zurich,
+ Stefan Leutenegger, Simon Lynen and Margarita Chli.
+
+ Copyright (C) 2013  The Autonomous Systems Lab, ETH Zurich,
+ Stefan Leutenegger and Simon Lynen.
+
+ BRISK - Binary Robust Invariant Scalable Keypoints
+ Reference implementation of
+ [1] Stefan Leutenegger,Margarita Chli and Roland Siegwart, BRISK:
+ Binary Robust Invariant Scalable Keypoints, in Proceedings of
+ the IEEE International Conference on Computer Vision (ICCV2011).
+
+ This file is part of BRISK.
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the <organization> nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef BRISKFEATURE_HPP_
+#define BRISKFEATURE_HPP_
+
+#include <brisk/brisk-opencv.h>
+#include <brisk/brisk-descriptor-extractor.h>
+#include <brisk/brisk-feature-detector.h>
+#include <brisk/harris-feature-detector.h>
+#include <brisk/harris-score-calculator.h>
+#include <brisk/scale-space-feature-detector.h>
+
+namespace brisk{
+
+class BriskFeature : public cv::Feature2D{
+public:
+	BriskFeature( size_t octaves, double uniformityRadius, double absoluteThreshold = 0,
+			size_t maxNumKpt = std::numeric_limits < size_t > ::max(),
+			bool rotationInvariant=true,
+            bool scaleInvariant=true) :
+            	_briskDetector(octaves, uniformityRadius, absoluteThreshold, maxNumKpt),
+            	_briskExtractor(rotationInvariant, scaleInvariant)
+            {}
+
+	virtual ~BriskFeature(){}
+
+	/* cv::DescriptorExtractor interface */
+	virtual int descriptorSize() const {return _briskExtractor.descriptorSize();}
+	virtual int descriptorType() const {return _briskExtractor.descriptorType();}
+
+	/* cv::Feature2D interface */
+	virtual void operator()( cv::InputArray image, cv::InputArray mask,
+	                                     std::vector<cv::KeyPoint>& keypoints,
+	                                     cv::OutputArray descriptors,
+	                                     bool useProvidedKeypoints=false ) const{
+		// handle provided keypoints correctly
+		if(!useProvidedKeypoints){
+			keypoints.clear();
+		}
+
+		// convert input output arrays:
+		cv::Mat descriptors_;
+		cv::Mat image_ = image.getMat();
+		cv::Mat mask_ = mask.getMat();
+
+		// detection
+		_briskDetector.detect(image_, keypoints, mask_); // this is already taking keypoints, if provided
+
+		// extraction
+		_briskExtractor.compute(image_, keypoints, descriptors_);
+		descriptors.getMatRef() = descriptors_;
+	}
+
+protected:
+
+	/* cv::FeatureDetector interface */
+	virtual void detectImpl( const cv::Mat& image, std::vector<cv::KeyPoint>& keypoints,
+			const cv::Mat& mask=cv::Mat() ) const{
+		_briskDetector.detect(image, keypoints, mask);
+	}
+
+	/* cv::DescriporExtractor interface */
+	virtual void computeImpl( const cv::Mat& image, std::vector<cv::KeyPoint>& keypoints,
+			cv::Mat& descriptors ) const{
+		_briskExtractor.computeImpl(image, keypoints, descriptors);
+	}
+
+	// we use members here instead of (triple) inheritance, in order to avoid diamonds of death
+	brisk::ScaleSpaceFeatureDetector<brisk::HarrisScoreCalculator> _briskDetector;
+	cv::BriskDescriptorExtractor _briskExtractor;
+
+};
+
+
+
+
+} // namespace brisk
+
+
+#endif /* BRISKFEATURE_HPP_ */
