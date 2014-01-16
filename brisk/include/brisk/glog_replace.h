a6f544a7d3961818316a29c9334f14a0a3900438
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-01-16 12:30:43 +0100

    adr comments

--- brisk/include/brisk/glog_replace.h
+++ brisk/include/brisk/glog_replace.h
@@ -2,6 +2,7 @@
 #define GLOG_REPLACE_H_
 
 #if !HAVE_GLOG
+#include <assert>
 #include <iosfwd>
 
 struct nullstream {};
@@ -14,14 +15,14 @@ inline nullstream & operator<<(nullstream& s, std::ostream&(std::ostream&)) {
 }
 static nullstream logstream;
 
-#define CHECK_NOTNULL(x) static_cast<void>(x);
+#define CHECK_NOTNULL(x) assert(x != nullptr);
 #define CHECK_EQ(x, y) assert(x == y); logstream
 #define CHECK_NE(x, y) assert(x != y); logstream
 #define CHECK_GT(x, y) assert(x > y); logstream
 #define CHECK_LT(x, y) assert(x < y); logstream
 #define CHECK_GE(x, y) assert(x >= y); logstream
 #define CHECK_LE(x, y) assert(x <= y); logstream
-#define CHECK(x) logstream
+#define CHECK(x) assert(x); logstream
 
 #endif  // !HAVE_GLOG
 #endif  // GLOG_REPLACE_H_
