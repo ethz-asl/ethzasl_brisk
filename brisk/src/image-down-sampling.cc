4932b2c603ec78d9a1cfa64dfb171e29477ecc03
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-01-20 21:57:27 +0100

    Fix bug downsampling

--- brisk/src/image-down-sampling.cc
+++ brisk/src/image-down-sampling.cc
@@ -699,11 +699,11 @@ void Twothirdsample8(const cv::Mat& srcimg, cv::Mat& dstimg) {
 #else
   // Masks:
   register __m128i mask1 = _mm_set_epi8(0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
-                                        0x80, 12, 0x80, 10, 0x80, 7, 0x80, 4,
+                                        0x80, 13, 0x80, 10, 0x80, 7, 0x80, 4,
                                         0x80, 1);
-  register __m128i mask2 = _mm_set_epi8(0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 12,
-                                        0x80, 10, 0x80, 7, 0x80, 4, 0x80, 1,
-                                        0x80);
+  register __m128i mask2 = _mm_set_epi8(0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+                                        13, 0x80, 10, 0x80, 7, 0x80, 4, 0x80,
+                                        1, 0x80);
   register __m128i mask = _mm_set_epi8(0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 14,
                                        12, 11, 9, 8, 6, 5, 3, 2, 0);
   register __m128i store_mask = _mm_set_epi8(0, 0, 0, 0, 0, 0, 0x80, 0x80, 0x80,
