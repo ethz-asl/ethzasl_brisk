052106317b11e8726fe6cea2c9182afe0ed05264
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-01-20 20:51:17 +0100

    twothirdsampling test visually ok, test fails with rounding diff

--- brisk/src/test/test-downsampling.cc
+++ brisk/src/test/test-downsampling.cc
@@ -92,6 +92,46 @@ void PlainHalfSample(const unsigned char* src, unsigned char* dst,
   }
 }
 
+void PlainTwoThirdSample(const unsigned char* src, unsigned char* dst,
+                         size_t src_rows, size_t src_cols) {
+  CHECK_NOTNULL(src);
+  CHECK_NOTNULL(dst);
+  const size_t dst_rows = src_rows / 3 * 2;
+  const size_t dst_cols = src_cols / 3 * 2;
+
+  for (size_t row = 0; row < dst_rows; row += 2) {
+     for (size_t col = 0; col < dst_cols; col += 2) {
+       const uint16_t A1 = src[(row / 2 * 3 + 0) * src_cols +
+                               (col / 2 * 3 + 0)];
+       const uint16_t A2 = src[(row / 2 * 3 + 0) * src_cols +
+                               (col / 2 * 3 + 1)];
+       const uint16_t A3 = src[(row / 2 * 3 + 0) * src_cols +
+                               (col / 2 * 3 + 2)];
+       const uint16_t B1 = src[(row / 2 * 3 + 1) * src_cols +
+                               (col / 2 * 3 + 0)];
+       const uint16_t B2 = src[(row / 2 * 3 + 1) * src_cols +
+                               (col / 2 * 3 + 1)];
+       const uint16_t B3 = src[(row / 2 * 3 + 1) * src_cols +
+                               (col / 2 * 3 + 2)];
+       const uint16_t C1 = src[(row / 2 * 3 + 2) * src_cols +
+                               (col / 2 * 3 + 0)];
+       const uint16_t C2 = src[(row / 2 * 3 + 2) * src_cols +
+                               (col / 2 * 3 + 1)];
+       const uint16_t C3 = src[(row / 2 * 3 + 2) * src_cols +
+                               (col / 2 * 3 + 2)];
+
+       dst[row * dst_cols + col] = static_cast<unsigned char>(
+               ((4 * A1 + 2 * (A2 + B1) + B2) / 9) & 0x00FF);
+       dst[row * dst_cols + col + 1] = static_cast<unsigned char>(
+               ((4 * A3 + 2 * (A2 + B3) + B2) / 9) & 0x00FF);
+       dst[(row + 1) * dst_cols + col] = static_cast<unsigned char>(
+           ((4 * C1 + 2 * (C2 + B1) + B2) / 9) & 0x00FF);
+       dst[(row + 1) * dst_cols + col + 1] = static_cast<unsigned char>(
+           ((4 * C3 + 2 * (C2 + B3) + B2) / 9) & 0x00FF);
+     }
+  }
+}
+
 TEST(Brisk, HalfSample) {
   std::string imagepath = "./src/test/test_data/img1.ppm";
   cv::Mat imgRGB = cv::imread(imagepath);
@@ -111,6 +151,28 @@ TEST(Brisk, HalfSample) {
   CheckImageSame(dst_img_a.data, dst_img_b.data, dst_rows, dst_cols);
 }
 
+TEST(Brisk, TwoThirdSample) {
+  std::string imagepath = "./src/test/test_data/img1.ppm";
+  cv::Mat imgRGB = cv::imread(imagepath);
+  cv::Mat src_img;
+  cv::cvtColor(imgRGB, src_img, CV_BGR2GRAY);
+
+  const static int source_cols = src_img.cols;
+  const static int source_rows = src_img.rows;
+  const static int dst_cols = source_cols / 3 * 2;
+  const static int dst_rows = source_rows / 3 * 2;
+  cv::Mat dst_img_a(dst_rows, dst_cols, CV_8UC1);
+  cv::Mat dst_img_b(dst_rows, dst_cols, CV_8UC1);
+
+  brisk::Twothirdsample8(src_img, dst_img_a);
+  PlainTwoThirdSample(src_img.data, dst_img_b.data, source_rows, source_cols);
+
+  cv::namedWindow("DownSample", cv::WINDOW_AUTOSIZE);
+  cv::imshow("DownSample", dst_img_b);
+  cv::waitKey(0);
+
+  CheckImageSame(dst_img_a.data, dst_img_b.data, dst_rows, dst_cols);
+}
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
