0c73c19f60044819bd85213dce8edc844831faba
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-01-21 01:20:49 +0100

    touch

--- brisk/src/test/test-binary-equal.h
+++ brisk/src/test/test-binary-equal.h
@@ -46,7 +46,7 @@ void RunPipeline(std::vector<DatasetEntry>& dataset,  // NOLINT
 bool RunVerification(const std::vector<DatasetEntry>& current_dataset,
                      const std::vector<DatasetEntry>& verification_dataset,
                      bool do_gtest_checks);
-void Draw(std::vector<DatasetEntry>& dataset);  // NOLINT
+void Draw(std::vector<DatasetEntry>& dataset);  // NOLINTr
 bool RunValidation(bool do_gtest_checks);
 }  // namespace brisk
 
