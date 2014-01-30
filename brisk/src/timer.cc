7a67541406f1670e21ed7b63799dc978410813d8
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-01-30 22:56:01 +0100

    touch

--- brisk/src/timer.cc
+++ brisk/src/timer.cc
@@ -201,9 +201,9 @@ inline int c99_snprintf(char* str, size_t size, const char* format, ...) {
 inline int c99_vsnprintf(char* str, size_t size, const char* format, va_list ap) {
   int count = -1;
   if (size != 0)
-  count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
+    count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
   if (count == -1)
-  count = _vscprintf(format, ap);
+    count = _vscprintf(format, ap);
   return count;
 }
 #endif  // _MSC_VER
