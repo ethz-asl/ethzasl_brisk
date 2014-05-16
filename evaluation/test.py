059e5c31c4268d9c26ba03351d85e1a54d020251
Mathias Bürki <mbuerki@mbuerki-W530.(none)>
2014-05-16 20:07:47 +0200

    added new files for the evaluatoin

--- evaluation/test.py
+++ evaluation/test.py
@@ -9,6 +9,12 @@ import Image
 
 import sm
 import aslam_cv as acv
+import ShelveWrapper as sw
+
+# Passing clearIntermediateResults will delete the file and start over
+db = sw.ShelveDb('test.shelve')
+
+frameTable = db.getTable('frames')
 
 sm.setLoggingLevel(sm.LoggingLevel.Debug)
 
@@ -24,23 +30,33 @@ cam_topic = '/mv_cameras_manager/GX005924/image_raw'
 
 gt_trans_list = []
 gt_rot_list = []
+i = 0
+
 for topic, msg, t in bagin.read_messages():
-  print topic
+  #print topic
   if topic == gt_topic:
     gt_trans = msg.transform.translation
     gt_rot = msg.transform.rotation
     gt_trans_list.append(gt_trans)
     gt_rot_list.append(gt_rot )
+    last_trans = gt_trans
+    last_rot = gt_rot
 
   if topic == cam_topic:
         img = Image.fromstring("L", [msg.width, msg.height], msg.data)
         stamp = acv.Time()
         stamp.fromNSec(t.to_nsec())
         mf = pipeline.addImage(stamp, 0, np.asanyarray(img))
-        IPython.embed()
-        for i in range(mf.numKeypoints()):
-          kp = mf.keypoint(i)
+        f = mf.getFrame(0)
+        f.setId(i)
+        keypoints = []
+        for i in range(f.numKeypoints()):
+          kp = f.keypoint(i)
           ray = kp.backProjection()
-        
+          keypoints.append((kp.y(), ray))
+
+        frameTable[i] = (f.image(), last_trans, last_rot, keypoints)
+        print 'stored frame ', i
+        i += 1
 
   
