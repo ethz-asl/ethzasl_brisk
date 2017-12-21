612cf719d99b55aba4e5cae0b8a19bf4332529dc
Fabian Blöchliger <f.bloechliger@gmail.com>
2017-12-21 17:45:40 +0100

    Updated export_flags.cmake to work with both armv7 and armv8.

--- agast/cmake/export_flags.cmake
+++ agast/cmake/export_flags.cmake
@@ -1,9 +1,17 @@
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DHAVE_OPENCV -std=c++0x")
-set(HAS_NEON FALSE)
-execute_process(COMMAND uname -m COMMAND tr -d '\n' OUTPUT_VARIABLE ARCH)
-if (ARCH MATCHES "^(arm)")
-  set(HAS_NEON TRUE)
-  add_definitions(-mfpu=neon)
-else()
+
+# Detect the preprocessor directives which are set by the compiler.
+execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dM -E -x c /dev/null
+                OUTPUT_VARIABLE PREPROCESSOR_DIRECTIVES)
+
+set(IS_SSE_ENABLED FALSE)
+if (PREPROCESSOR_DIRECTIVES MATCHES "__SSE2__")
   add_definitions(-mssse3)
+  set(SIMD_SSE 1)
+  set(IS_SSE_ENABLED TRUE)
+# For both armv7 and armv8, __ARM_NEON is used as preprocessor directive.
+elseif (PREPROCESSOR_DIRECTIVES MATCHES "__ARM_ARCH 7")
+  add_definitions(-mfpu=neon) # Needs to be set for armv7.
+elseif (PREPROCESSOR_DIRECTIVES MATCHES "__ARM_ARCH 8")
+  # Add other potential compiler flags here.
 endif()
